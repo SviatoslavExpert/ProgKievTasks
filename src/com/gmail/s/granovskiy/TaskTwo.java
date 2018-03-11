73d06ee93e86d18f70f6fe4ee4e21795727815ac
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-11 11:58:46 +0200

    task

--- src/com/gmail/s/granovskiy/TaskTwo.java
+++ src/com/gmail/s/granovskiy/TaskTwo.java
@@ -5,7 +5,7 @@ public class TaskTwo {
         double a = 0.3;
         double b = 0.4;
         double c = 0.5;
-        double p = (a + b + c)/2;
+        double p = (a + b + c)/2.0;
         System.out.println("Площадь треугольника: " + Math.sqrt(p*(p - a)*(p - b)*(p - c)));
     }
 }
