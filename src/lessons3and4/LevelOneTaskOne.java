34ed94e005a3e3d6a9c86d0bb50151f576d576eb
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-13 09:48:44 +0200

    task

---
+++ src/lessons3and4/LevelOneTaskOne.java
@@ -0,0 +1,25 @@
+package lessons3and4;
+
+import java.util.Scanner;
+
+public class LevelOneTaskOne {
+
+    public static void main(String []args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите первое число:");
+        int a = sc.nextInt();
+        System.out.println("Введите второе число:");
+        int b = sc.nextInt();
+        System.out.println("Введите третье число:");
+        int c = sc.nextInt();
+        System.out.println("Введите четвертое число:");
+        int d = sc.nextInt();
+
+        System.out.println(select(select(a,b), select(c,d)));
+    }
+
+    private static int select(int x, int y){
+        if (x > y) return x;
+        else return y;
+    }
+}
