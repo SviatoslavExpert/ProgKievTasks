dd59928a833b16a04ae90b74a77e4ed9fae35658
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-14 09:58:34 +0200

    task

--- src/lesson4/LevelOneTaskOne.java
+++ src/lesson4/LevelOneTaskOne.java
@@ -1,12 +1,37 @@
 /*
-
+С помощью циклов нарисовать «обои». Причем количество полос
+должно вводиться с клавиатуры. В примере 7 полос.
+***+++***+++***+++***
+***+++***+++***+++***
+***+++***+++***+++***
+***+++***+++***+++***
+***+++***+++***+++***
  */
 
 package lesson4;
 
-
+import java.util.Scanner;
 
 public class LevelOneTaskOne {
 
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите количество полос:");
+        int rows = sc.nextInt();
+        System.out.println("Введите длину полос:");
+        int height = sc.nextInt();
+
+        for (int i = 0; i < height; i++){
+            for (int j = 0; j < rows; j++){
+                if(j%2 ==0){
+                    System.out.print("***");
+                }
+                else {
+                    System.out.print("+++");
+                }
+            }
+            System.out.println();
+        }
+    }
 
 }
