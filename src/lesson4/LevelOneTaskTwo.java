7d504a8cd0387f6223339a513b72498615869ddf
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-14 09:47:46 +0200

    task

--- src/lesson4/LevelOneTaskTwo.java
+++ src/lesson4/LevelOneTaskTwo.java
@@ -6,5 +6,37 @@
 
 package lesson4;
 
+import java.util.Scanner;
+
 public class LevelOneTaskTwo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите координату Х");
+        double x = sc.nextDouble();
+        System.out.print("Введите координату Y");
+        double y = sc.nextDouble();
+
+        double ax = 0;
+        double ay = 0;
+        double bx = 4;
+        double by = 4;
+        double cx = 6;
+        double cy = 1;
+
+        double k1 = (ax - x) * (by - ay) - (bx - ax) * (ay - y);
+        double k2 = (bx - x) * (cy - by) - (cx - bx) * (by - y);
+        double k3 = (cx - x) * (ay - cy) - (ax - cx) * (cy - y);
+
+        if(k1 > 0 && k2 > 0 && k3 > 0 || k1 < 0 && k2 < 0 && k3 < 0){
+            System.out.println("Точка лежит в треугольнике.");
+        }
+        else if(k1 == 0 && k2 == 0 && k3 == 0){
+            System.out.println("Точка лежит на стороне треугольника.");
+        }
+        else{
+            System.out.println("Точка не лежит в треугольнике.");
+        }
+        sc.close();
+    }
 }
