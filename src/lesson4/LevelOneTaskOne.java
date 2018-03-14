2e5f2a48fced0ca78820aade11e10b76d199ec62
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-14 09:26:58 +0200

    task

--- src/lesson4/LevelOneTaskOne.java
+++ src/lesson4/LevelOneTaskOne.java
@@ -6,5 +6,29 @@
 
 package lesson4;
 
+import java.util.Scanner;
+
 public class LevelOneTaskOne {
+
+    public static void main (String []args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите значение координаты X точки:");
+        double pointX = sc.nextDouble();
+        System.out.println("Введите значение координаты Y точки:");
+        double pointY = sc.nextDouble();
+
+        double circleX = 0;
+        double circleY = 0;
+        double radius = 4;
+        double result = Math.sqrt(Math.pow((pointX - circleX), 2) + Math.pow((pointY - circleY), 2));
+
+        if (result < radius) {
+            System.out.println("Точка лежит внутри круга.");
+        }
+        else if(result == radius) {
+            System.out.println("Точка лежит на линии круга.");
+        }
+        else System.out.println("Точка лежит вне круга.");
+        sc.close();
+    }
 }
