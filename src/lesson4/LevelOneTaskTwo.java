33fa9684861b16a70f1e81e25ce32013a84f1def
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-14 10:02:24 +0200

    task

--- src/lesson4/LevelOneTaskTwo.java
+++ src/lesson4/LevelOneTaskTwo.java
@@ -1,16 +1,10 @@
 /*
-Напечатайте таблицу умножения на 5. предпочтительно печатать 1 x 5 =
-5, 2 x 5 = 10, а не просто 5, 10 и т. д.
+
  */
 
 package lesson4;
 
 public class LevelOneTaskTwo {
 
-    public static void main (String []args){
-        int num = 10;
-        for (int i = 1; i < num; i++){
-            System.out.println(i + " x " + 5 + " = " + i*5);
-        }
-    }
+
 }
