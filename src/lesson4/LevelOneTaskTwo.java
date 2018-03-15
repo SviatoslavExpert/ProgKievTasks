4abbad659794b193801315d3286b2b2af6e8e350
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-15 07:41:47 +0200

    task

--- src/lesson4/LevelOneTaskTwo.java
+++ src/lesson4/LevelOneTaskTwo.java
@@ -1,10 +1,32 @@
 /*
-
+Вычислить с помощью цикла факториал числа - n введенного с
+клавиатуры (4<n<16). Факториал числа это произведение всех чисел от
+этого числа до 1. Например 5!=5*4*3*2*1=120
  */
 
 package lesson4;
 
+import java.util.Scanner;
+
 public class LevelOneTaskTwo {
 
+    public static void main (String []args){
 
+        //  Scanning the number
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число от 4-х до 16:");
+        int number = sc.nextInt();
+
+        //  meeting the condition 4 < n < 16
+        //  calculating and printing factorial of n
+        int x = 1;
+        if(number > 4 && number < 16) {
+            for (int i = 1; i <= number; i++) {
+                x *= i;
+            }
+            System.out.println("Факториал числа " + number + ": " + x);
+        }
+        else System.out.println("Неверное число.");
+        sc.close();
+    }
 }
