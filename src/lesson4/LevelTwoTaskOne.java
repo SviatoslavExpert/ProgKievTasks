7810716e28f0cf11f427ffe764ce6086fd8aed40
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-16 08:27:22 +0200

    task

---
+++ src/lesson4/LevelTwoTaskOne.java
@@ -0,0 +1,37 @@
+/*
+С помощью цикла (только одного) нарисовать такую фигуру. Причем
+максимальная высота этой фигуры вводиться с клавиатуры (в примере
+максимальная высота - 4)
+*
+**
+***
+****
+***
+**
+*
+ */
+package lesson4;
+
+import java.util.Scanner;
+
+public class LevelTwoTaskOne {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите максимальную высоту: ");
+        int num = sc.nextInt()*2;
+
+        int j = 0;
+        for(int i = 0; i < (num - 1) ;){
+            if(j >= (num/2) - Math.abs(num/2 - i)){
+                System.out.println();
+                i++;
+                j = 0;
+            }
+            System.out.print("*");
+            j++;
+        }
+        sc.close();
+    }
+}
