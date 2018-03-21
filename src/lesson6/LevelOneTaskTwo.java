de05ff56b451c4943612fe982e938695d4914b1d
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-21 14:15:14 +0200

    task

---
+++ src/lesson6/LevelOneTaskTwo.java
@@ -0,0 +1,17 @@
+/*
+Реализуйте метод параметрами которого являются - целое число,
+вещественное число и строка. Возвращает он конкатенацию строки с
+суммой вещественного и целого числа.
+ */
+
+package lesson6;
+
+public class LevelOneTaskTwo {
+    public static void main(String[] args){
+        System.out.println(returnConcatenation(5, 3.71, "word"));
+    }
+
+    private static String returnConcatenation(int a, double b, String s){
+        return s + (String.valueOf(a + b));
+    }
+}
