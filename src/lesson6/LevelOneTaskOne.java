49fc6b6501c0f83694cabb45595a39501533f1bd
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-21 14:09:12 +0200

    task

---
+++ src/lesson6/LevelOneTaskOne.java
@@ -0,0 +1,24 @@
+/*
+Напишите метод который вернет максимальное число из массива целых
+чисел.
+ */
+
+package lesson6;
+
+public class LevelOneTaskOne {
+
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+        System.out.println("Максимальное число из массива: " + searchMax(arr));
+    }
+
+    private static int searchMax(int[] arr){
+        int maxNumber = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(maxNumber < arr[i]){
+                maxNumber = arr[i];
+            }
+        }
+        return maxNumber;
+    }
+}
