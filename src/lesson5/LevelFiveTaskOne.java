a7e5c7510ea6431150bbfefc6077df995b627c5a
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-21 11:33:47 +0200

    task

---
+++ src/lesson5/LevelFiveTaskOne.java
@@ -0,0 +1,22 @@
+/*
+Дан массив {0,5,2,4,7,1,3,19} — написать программу для подсчета
+нечетных цифр в нем.
+ */
+
+package lesson5;
+
+public class LevelFiveTaskOne {
+
+    public static void main(String[] args){
+
+        int [] array = {0,5,2,4,7,1,3,19};
+
+        int counter = 0;
+        for(int i = 0; i < array.length; i++){
+            if(array[i]%2 != 0){
+                counter++;
+            }
+        }
+        System.out.println("Количество нечетных цифр в массиве: " + counter);
+    }
+}
