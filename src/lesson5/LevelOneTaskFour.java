afc85aa334a1051f3acf973e5dd70b17786f6dfb
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-21 13:01:46 +0200

    task

--- src/lesson5/LevelOneTaskFour.java
+++ src/lesson5/LevelOneTaskFour.java
@@ -12,14 +12,14 @@ public class LevelOneTaskFour {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку: ");
         String s = sc.nextLine();
-        char b = b;
 
         char[] arrayOfCharacters = s.toCharArray(); // created an array o characters
 
         int counter = 0;
         for(int i = 0; i < arrayOfCharacters.length; i++){
-            if(arrayOfCharacters[i] == b){
+            if(arrayOfCharacters[i] == 'b'){
                 counter++;
             }
         }
