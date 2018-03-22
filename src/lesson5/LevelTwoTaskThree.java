09431f51565694356287c4e52ddc86928c57dcef
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-22 12:12:07 +0200

    task

---
+++ src/lesson5/LevelTwoTaskThree.java
@@ -0,0 +1,14 @@
+/*
+Ввести с клавиатуры число (до миллиарда) которое обозначает
+количество долларов и центов пользователя. Вывести это количество
+прописью. (4 часа)
+Например:
+How much money do you have?
+123,34
+You have: one hundred twenty three dollars thirty four cents
+ */
+
+package lesson5;
+
+public class LevelTwoTaskThree {
+}
