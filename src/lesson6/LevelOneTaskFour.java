bcb6b20d90ae79be1c4af67b589e4f24ab09b3d6
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-22 08:47:01 +0200

    task

---
+++ src/lesson6/LevelOneTaskFour.java
@@ -0,0 +1,30 @@
+/*
+Напишите метод который реализует линейный поиск элемента в массиве
+целых чисел. Если такой элемент в массиве есть то верните его индекс,
+если нет то метод должен возвращать число - «-1»
+ */
+
+package lesson6;
+
+import java.util.Scanner;
+
+public class LevelOneTaskFour {
+    public static void main(String[] args){
+        int[] arr = {1,2,3,4,5,6,7,8,9,145, 557, 345, 654, 999};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите елемент массива, индекс которого вы хотите найти: ");
+        int a = sc.nextInt();
+
+        System.out.println(searchElement(a,arr));
+    }
+
+    private static int searchElement(int a, int[] arr){
+        int result = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == a){
+                result = i;
+            }
+        }
+        return result;
+    }
+}
