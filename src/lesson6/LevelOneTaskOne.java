79e9a88b7f092463c89b1102429a813998565abb
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-26 09:26:30 +0300

    task

--- src/lesson6/LevelOneTaskOne.java
+++ src/lesson6/LevelOneTaskOne.java
@@ -8,12 +8,12 @@ package lesson6;
 public class LevelOneTaskOne {
 
     public static void main(String[] args){
-        int[] arr = {1,2,3,4,5,6,7,8,9};
+        int[] arr = {-5,-100789,-2147483647,2,3,4,5,6,7,8,9,2147483647};
         System.out.println("Максимальное число из массива: " + searchMax(arr));
     }
 
     private static int searchMax(int[] arr){
-        int maxNumber = 0;
+        int maxNumber = -2147483648;
         for(int i = 0; i < arr.length; i++){
             if(maxNumber < arr[i]){
                 maxNumber = arr[i];
