f0eaf265164fa4b6151f6832987dcfac9cd5cd67
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-27 19:58:35 +0300

    task

--- src/lesson6/LevelTwoTaskOneB.java
+++ src/lesson6/LevelTwoTaskOneB.java
@@ -16,7 +16,8 @@ import java.util.Scanner;
 
 public class LevelTwoTaskOneB {
     public static void main(String[] args) {
-        sortAndPrint(scanNumbers());
+
+        printNumber((checkSequence(scanNumbers())), scanNumbers());
     }
 
     private static int[] scanNumbers() {
@@ -35,7 +36,60 @@ public class LevelTwoTaskOneB {
         return arr;
     }
 
-    private static void sortAndPrint(int[] arr) {
+    private static int[] checkSequence(int[] arr) {
+        int countOne = 0;
+        int countTwo = 0;
+        int countThree = 0;
+        int countFour = 0;
+        int countFive = 0;
+        int countSix = 0;
+        for(int i = 0; i < (arr.length - 1); i++){
+            if(arr[i] + 2 == arr[i + 1]){
+                ++countOne;
+            }
+            if(arr[i] + 3 == arr[i + 1]){
+                ++countTwo;
+            }
+            if(arr[i] * 2 == arr[i + 1]){
+                ++countThree;
+            }
+            if(arr[i] * 3 == arr[i + 1]){
+                ++countFour;
+            }
+            if(Math.pow(arr[i], 2) == arr[i + 1]){
+                ++countFive;
+            }
+            if(Math.pow(arr[i], 3) == arr[i + 1]){
+                ++countSix;
+            }
+        }
+        int[] counts = {countOne, countTwo, countThree, countFour, countFive, countSix};
+        return counts;
+    }
+
+    private static void printNumber(int[] counts, int[]arr){
+        if(counts[0] >= 4 ){
+            System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] + 2));
+        }
+        if(counts[1] >= 4 ){
+            System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] + 3));
+        }
+        if(counts[1] >= 4 ){
+            System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] * 2));
+        }
+        if(counts[1] >= 4 ){
+            System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] * 3));
+        }
+        if(counts[1] >= 4 ){
+            System.out.println("Следующее число в этой последовательности: " + (Math.pow(arr[arr.length - 1], 2)));
+        }
+        if(counts[1] >= 4 ){
+            System.out.println("Следующее число в этой последовательности: " + (Math.pow(arr[arr.length - 1], 3)));
+        }
+    }
+}
+
+/*
         if (arr[0] + 2 == arr[1] && arr[1] + 2 == arr[2] && arr[2] + 2 == arr[3]) {
             System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] + 2));
         } else if (arr[0] + 3 == arr[1] && arr[1] + 3 == arr[2] && arr[2] + 3 == arr[3]) {
@@ -43,7 +97,7 @@ public class LevelTwoTaskOneB {
         } else if (arr[0] * 2 == arr[1] && arr[1] * 2 == arr[2] && arr[2] * 2 == arr[3]) {
             System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] * 2));
         } else if (arr[0] * 3 == arr[1] && arr[1] * 3 == arr[2] && arr[2] * 3 == arr[3]) {
-            System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] * 2));
+            System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] * 3));
         } else if ((Math.pow(arr[0], 2) == 1) && (Math.pow(arr[1], 2) == 4) && (Math.pow(arr[2], 2) == 9)) {
             System.out.println("Следующее число в этой последовательности: " + (Math.pow(arr[arr.length - 1], 2)));
         } else if ((Math.pow(arr[0], 3) == 1) && (Math.pow(arr[1], 3) == 8) && (Math.pow(arr[2], 3) == 27)) {
@@ -51,5 +105,4 @@ public class LevelTwoTaskOneB {
         }else{
             System.out.println("Неизвестная последовательность чисел.");
         }
-    }
-}
+ */
