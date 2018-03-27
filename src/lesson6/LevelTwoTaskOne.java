0cf547f9f0fc583e07168db4be878accd85ef7a6
SviatoslavExpert <s.granovskiy@gmail.com>
2018-03-27 14:34:03 +0300

    task

--- src/lesson6/LevelTwoTaskOne.java
+++ src/lesson6/LevelTwoTaskOne.java
@@ -15,32 +15,41 @@ package lesson6;
 import java.util.Scanner;
 
 public class LevelTwoTaskOne {
-    public static void main(String[] args){
-        getPattern(scanNumbers());
+    public static void main(String[] args) {
+        sortAndPrint(scanNumbers());
     }
 
-    private static int[] scanNumbers(){
+    private static int[] scanNumbers() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Необходимо ввести не менее 4-х чисел. Укажите, сколько чисел вы собираетесь ввести: ");
         int numbers = sc.nextInt();
 
-        if(numbers >= 4) {
-            int[] arr = new int[numbers];
+        int[] arr = new int[numbers];
 
-            int num = 0;
-            for (int i = 0; i < numbers; i++) {
-                System.out.println("Введите число № " + i);
-                num = sc.nextInt();
-                arr[i] = num;
-            }
-            return arr;
-        } else {
-            System.out.println("Вы ввели недостаточное количество чисел.");
+        int num = 0;
+        for (int i = 0; i < numbers; i++) {
+            System.out.println("Введите число №" + (i + 1));
+            num = sc.nextInt();
+            arr[i] = num;
         }
-
+        return arr;
     }
 
-    private static int getPattern(int[] arr){
-
+    private static void sortAndPrint(int[] arr) {
+        if (arr[0] + 2 == arr[1] && arr[1] + 2 == arr[2] && arr[2] + 2 == arr[3]) {
+            System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] + 2));
+        } else if (arr[0] + 3 == arr[1] && arr[1] + 3 == arr[2] && arr[2] + 3 == arr[3]) {
+            System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] + 3));
+        } else if (arr[0] * 2 == arr[1] && arr[1] * 2 == arr[2] && arr[2] * 2 == arr[3]) {
+            System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] * 2));
+        } else if (arr[0] * 3 == arr[1] && arr[1] * 3 == arr[2] && arr[2] * 3 == arr[3]) {
+            System.out.println("Следующее число в этой последовательности: " + (arr[arr.length - 1] * 2));
+        } else if ((Math.pow(arr[0], 2) == 1) && (Math.pow(arr[1], 2) == 4) && (Math.pow(arr[2], 2) == 9)) {
+            System.out.println("Следующее число в этой последовательности: " + (Math.pow(arr[arr.length - 1], 2)));
+        } else if ((Math.pow(arr[0], 3) == 1) && (Math.pow(arr[1], 3) == 8) && (Math.pow(arr[2], 3) == 27)) {
+            System.out.println("Следующее число в этой последовательности: " + (Math.pow(arr[arr.length - 1], 3)));
+        }else{
+            System.out.println("Неизвестная последовательность чисел.");
+        }
     }
 }
