70dcebd0e85a2c4288312029098089ad2c1a7304
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-02 10:40:23 +0300

    task

--- src/lesson7/LevelOneTaskFour.java
+++ src/lesson7/LevelOneTaskFour.java
@@ -1,8 +1,22 @@
 /*
-
+Выведите на экран 10 строк со значением числа Пи. Причем в первой
+строке должно быть 2 знака после запятой, во второй 3, в третьей 4 и т.д.
  */
 
 package lesson7;
 
+import java.util.Formatter;
+
 public class LevelOneTaskFour {
+
+    public static void main(String[] args){
+
+        int number = 10;
+        for(int i = 2; i < (number + 2); i++) {
+            Formatter fr = new Formatter();
+            fr.format("%." + i + "f", Math.PI);
+
+            System.out.println(fr.toString());
+        }
+    }
 }
