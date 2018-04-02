5b5a3fc2c767069e89c4d82da6770a0e778fc5cb
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-02 09:31:01 +0300

    task

--- src/lesson7/LevelOneTaskOne.java
+++ src/lesson7/LevelOneTaskOne.java
@@ -1,4 +1,27 @@
+
+
 package lesson7;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class LevelOneTaskOne {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Calendar calOne = Calendar.getInstance();
+        Calendar calTwo = Calendar.getInstance();
+
+        calTwo.set(Calendar.MONTH, calOne.get(Calendar.MONTH) - 1);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yy");
+
+        Date dateOne = calOne.getTime();
+        Date dateTwo = calTwo.getTime();
+
+        long t = dateOne.getTime() - dateTwo.getTime();
+
+        System.out.println("From " + sdf.format(calOne.getTime()) + " to " + sdf.format(calTwo.getTime()) + ":");
+        System.out.println(t + " milliseconds");
+    }
 }
