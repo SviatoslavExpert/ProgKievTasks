640cffab5b05eadd12cd995b41774cf1abc3af6e
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-13 08:27:24 +0300

    task

--- src/lesson8/LevelOneExampleTwo.java
+++ src/lesson8/LevelOneExampleTwo.java
@@ -1,4 +1,15 @@
 package lesson8;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class LevelOneExampleTwo {
+
+    public static void main(String[] args){
+        String text = loadFromFile(new File("a.txt"));
+        System.out.println(text);
+    }
+
+
 }
