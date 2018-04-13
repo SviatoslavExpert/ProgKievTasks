2bdb35fac56e4a6e478270a66f95f9cc7bfdf77e
SviatoslavExpert <s.granovskiy@gmail.com>
2018-04-13 08:14:58 +0300

    task

--- src/lesson8/LevelOneExample.java
+++ src/lesson8/LevelOneExample.java
@@ -1,4 +1,19 @@
 package lesson8;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class LevelOneExample {
+    public static void main(String[] args){
+        try (PrintWriter a = new PrintWriter("c.txt")){
+            for(int i = 0; i < 10; i++){
+                a.print(i);
+            }
+            a.println();
+
+            a.println("Hello World!");
+        } catch(FileNotFoundException e) {
+
+        }
+    }
 }
