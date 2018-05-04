ee779ff055c50ae561e2d756b110f1d1f71f720b
SviatoslavExpert <s.granovskiy@gmail.com>
2018-05-04 10:38:08 +0300

    task

---
+++ src/lesson8/LevelOneTaskOne.java
@@ -0,0 +1,12 @@
+/*
+1) Создайте консольный «текстовый редактор» с возможностью
+сохранения набранного текста в файл.
+ */
+
+package lesson8;
+
+public class LevelOneTaskOne {
+    public static void mian(String[] args){
+
+    }
+}
